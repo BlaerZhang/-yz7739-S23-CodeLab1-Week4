e667dfceaa3ef08d4de5afe901e08c0f281d2ec3
BlaerZhang <zhangyueyang0022@126.com>
2023-02-25 03:55:56 -0500

    Disabled Desktop File Function; Added a speed run score list, check /DATA/HighScores.txt

--- CCTVMaze/Assets/Scripts/EndingTrigger.cs
+++ CCTVMaze/Assets/Scripts/EndingTrigger.cs
@@ -5,6 +5,7 @@ using DG.Tweening;
 using UnityEngine;
 using TMPro;
 using System.IO;
+using Unity.VisualScripting;
 
 public class EndingTrigger : MonoBehaviour
 {
@@ -12,44 +13,52 @@ public class EndingTrigger : MonoBehaviour
     
     private bool hitten = false;
 
-    private string systemLog =
-        System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Ipconfig Smart Plug B09HPW" + "     Permitted" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Ping 192.168.122.235/24" + "            Permitted" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Permitted" + "\n"
-        +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      "+Environment.UserName+", isn't it?" + "\n"
-        +System.DateTime.Now.ToString() + "     👁👁👁👁👁" + "       👁👁👁👁" + "      👁👁👁👁👁👁👁👁👁👁👁" + "     👁👁👁👁"; //content of the txt file
+    private float score = 0;
+
+    private List<float> highScores = new List<float>();
+
+    private string FILE_PATH;
+    private const string FILE_NAME = "HighScores.txt";
+    private const string FILE_DIR = "/DATA/";
+
+    // private string systemLog =
+    //     System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Denied" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Ipconfig Smart Plug B09HPW" + "     Permitted" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Ping 192.168.122.235/24" + "            Permitted" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      Request WebCam Permission" + "      Permitted" + "\n"
+    //     +System.DateTime.Now.ToString() + "     CCTVMaze.exe" + "       anonymous" + "      "+Environment.UserName+", isn't it?" + "\n"
+    //     +System.DateTime.Now.ToString() + "     👁👁👁👁👁" + "       👁👁👁👁" + "      👁👁👁👁👁👁👁👁👁👁👁" + "     👁👁👁👁"; //content of the txt file
     
     // Start is called before the first frame update
     void Start()
     {
-        // endingText.DOFade(0, 0); //set text alpha to 0
+        FILE_PATH = Application.dataPath + FILE_DIR + FILE_NAME;
     }
 
     // Update is called once per frame
@@ -64,13 +73,47 @@ public class EndingTrigger : MonoBehaviour
         {
             if (hitten == false) //if not hitten before
             {
+                UpdateHighScore();
                 endingText = GameObject.Find("EndingText").GetComponent<TextMeshProUGUI>(); //find TextMeshProUGUI in the scene
-                endingText.text = "The End"; //set text in UI
+                endingText.text = "The End\n" + File.ReadAllText(FILE_PATH); //set text in UI
                 endingText.DOFade(0, 0); //set text alpha to 0
                 endingText.DOFade(1, 3); //text fade in
-                File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"/System Log.txt", systemLog); //create a txt file on desktop
+                // File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"/System Log.txt", systemLog); //create a txt file on desktop
                 hitten = true; //set status to hitten
             }
         }
+    }
+
+    private void UpdateHighScore() //Update High Score Function
+    {
+        score = Time.timeSinceLevelLoad; //record current score
+
+        //read high scores from txt
+        if (highScores.Count == 0)
+        {
+            string[] fileSplit = File.ReadAllText(FILE_PATH).Split("\n");
+            for (int i = 1; i < fileSplit.Length - 1; i++) 
+            {
+                highScores.Add(float.Parse(fileSplit[i]));
+            }
+        }
+        
+        //insert current score into high score
+        for (int i = 0; i < highScores.Count; i++)
+        {
+            if (score <= highScores[i]) 
+            {
+                highScores.Insert(i, score);
+                break;
+            }
+        }
+
+        //write high scores into txt
+        string highScoresText = "High Scores:\n";
+        for (int i = 0; i < highScores.Count; i++)
+        {
+            highScoresText += highScores[i] + "\n";
+        }
+        File.WriteAllText(FILE_PATH, highScoresText);
     }
 }
